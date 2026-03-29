b9a520d8cc44033038636146f201449088986765
Nisarga M <nisargam73@gmail.com>
2026-03-29 22:12:36 +0530

    Create if else.py

---
+++ if else.py
@@ -0,0 +1,16 @@
+num=int(input("Enter a number:"))
+if num%2==0:
+    print ("Even number")
+else:
+    print("Odd number")
+
+
+marks=int(input("Enter your marks: "))
+ if marks>=90:
+    print("Grade: A")
+ elif marks>= 80:
+    print("Grade: B")
+ elif marks>=70:
+    print("Grade: C")
+ else:
+   print("try to work hard")
