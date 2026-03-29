48573aa98bd50de3f316fcd2231b54cfa416d61a
Nisarga M <nisargam73@gmail.com>
2026-03-29 22:40:08 +0530

    Fix image path in grayscale.py

--- grayscale.py
+++ grayscale.py
@@ -1,5 +1,5 @@
 import cv2
-img=cv2.imread(r"C:\Users\91701\Pictures\Screenshots\Screenshot (217).png")
+img=cv2.imread(r"C:\Users\Pictures\Screenshots\Screenshot (217).png")
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 blur=cv2.GaussianBlur(gray,(5,5),0)
 cv2.imshow("Blurred Image",blur)
